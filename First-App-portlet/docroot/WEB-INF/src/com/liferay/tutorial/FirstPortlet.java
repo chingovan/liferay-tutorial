668a1a8d64baf76ea4f0449cb37a22e016ca49b1
programmerit <chingovan@gmail.com>
2015-05-31 08:32:26 +0700

    First commit

---
+++ First-App-portlet/docroot/WEB-INF/src/com/liferay/tutorial/FirstPortlet.java
@@ -0,0 +1,11 @@
+package com.liferay.tutorial;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class FirstPortlet
+ */
+public class FirstPortlet extends MVCPortlet {
+ 
+
+}
